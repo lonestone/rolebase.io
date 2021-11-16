57bf29e609ae44adb47cf09361302c630fb6e767
Godefroy <git@skreo.net>
2021-11-16 21:35:43 +0100

    Fix react fast refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 })
