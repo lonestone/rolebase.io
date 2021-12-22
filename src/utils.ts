79e845d7f44f4696f3adec4126e3b1aad687bb1f
Godefroy <git@skreo.net>
2021-12-22 19:37:12 +0100

    Fix getDateTimeLocal

--- src/utils.ts
+++ src/utils.ts
@@ -6,5 +6,5 @@ export function capitalizeFirstLetter(str: string) {
 
 // From https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local
 export function getDateTimeLocal(date: Date) {
-  return format(date, "yyyy-MM-dd'T'hh:mm")
+  return format(date, "yyyy-MM-dd'T'HH:mm", {})
 }
