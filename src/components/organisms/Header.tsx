a6c31129046502152447dc98c8a8bf10ae5bca76
Kérinec Fabien <fabien@lonestone.studio>
2022-02-22 18:11:24 +0100

    open modal meeting

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,11 @@
-import { Flex, Heading, Spacer, useColorMode, Text } from '@chakra-ui/react'
+import {
+  Flex,
+  Heading,
+  Spacer,
+  useColorMode,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
 import HeaderButton from '@components/atoms/HeaderButton'
 import OrgMenu from '@components/molecules/OrgMenu'
 import HeaderSearchCombobox from '@components/molecules/search/HeaderSearchCombobox'
@@ -13,78 +20,84 @@ import {
   FiMessageSquare,
 } from 'react-icons/fi'
 import useCurrentMember from '@hooks/useCurrentMember'
+import MeetingModal from './modals/MeetingModal'
 
 export default function Header() {
   const user = useStoreState((state) => state.auth.user)
   const org = useCurrentOrg()
   const member = useCurrentMember()
 
+  const {
+    isOpen: isMeetingOpen,
+    onOpen: openMeetingModal,
+    onClose: closeMeetingModal,
+  } = useDisclosure()
+
   const { colorMode } = useColorMode()
 
   if (!user) return null
   return (
-    <Flex
-      w="100%"
-      h="48px"
-      alignItems="center"
-      px={1}
-      bg={colorMode === 'light' ? 'gray.100' : 'gray.700'}
-      borderBottom="1px solid"
-      borderBottomColor={colorMode === 'light' ? 'gray.200' : 'gray.550'}
-      zIndex={1000}
-    >
-      {org && (
-        <>
-          <OrgMenu />
+    <>
+      <Flex
+        w="100%"
+        h="48px"
+        alignItems="center"
+        px={1}
+        bg={colorMode === 'light' ? 'gray.100' : 'gray.700'}
+        borderBottom="1px solid"
+        borderBottomColor={colorMode === 'light' ? 'gray.200' : 'gray.550'}
+        zIndex={1000}
+      >
+        {org && (
+          <>
+            <OrgMenu />
 
-          <Heading size="sm" ml={5} mr={5}>
-            {org.name}
-          </Heading>
+            <Heading size="sm" ml={5} mr={5}>
+              {org.name}
+            </Heading>
 
-          <HeaderButton exact to={`/orgs/${org.id}`} leftIcon={<FiDisc />}>
-            Cercles
-          </HeaderButton>
+            <HeaderButton exact to={`/orgs/${org.id}`} leftIcon={<FiDisc />}>
+              Cercles
+            </HeaderButton>
 
-          <HeaderButton
-            to={`/orgs/${org.id}/threads`}
-            leftIcon={<FiMessageSquare />}
-          >
-            Discussions
-          </HeaderButton>
+            <HeaderButton
+              to={`/orgs/${org.id}/threads`}
+              leftIcon={<FiMessageSquare />}
+            >
+              Discussions
+            </HeaderButton>
 
-          <HeaderButton
-            to={`/orgs/${org.id}/meetings`}
-            leftIcon={<FiCalendar />}
-          >
-            Réunions
-          </HeaderButton>
+            <HeaderButton
+              to={`/orgs/${org.id}/meetings`}
+              leftIcon={<FiCalendar />}
+            >
+              Réunions
+            </HeaderButton>
 
-          <HeaderButton
-            to={`/orgs/${org.id}/tasks`}
-            leftIcon={<FiCheckSquare />}
-          >
-            Tâches
-          </HeaderButton>
-        </>
-      )}
-      <Spacer />
-      {member?.meetingId && (
-        <Text
-          colorScheme="blue"
-          mr="2"
-          px="2"
-          py="1"
-          bg={'blue.300'}
-          borderRadius="8"
-          fontWeight="semibold"
-          color={colorMode === 'light' ? 'white' : 'black'}
-          whiteSpace="nowrap"
-        >
-          En cours de meeting
-        </Text>
-      )}
-      {org && <HeaderSearchCombobox />}
-      <UserMenu ml={2} />
-    </Flex>
+            <HeaderButton
+              to={`/orgs/${org.id}/tasks`}
+              leftIcon={<FiCheckSquare />}
+            >
+              Tâches
+            </HeaderButton>
+          </>
+        )}
+        <Spacer />
+        {member?.meetingId && (
+          <Button size="sm" colorScheme="blue" onClick={openMeetingModal}>
+            Réunion en cours
+          </Button>
+        )}
+        {org && <HeaderSearchCombobox />}
+        <UserMenu ml={2} />
+      </Flex>
+      {isMeetingOpen && member?.meetingId ? (
+        <MeetingModal
+          id={member.meetingId}
+          isOpen
+          onClose={closeMeetingModal}
+        />
+      ) : null}
+    </>
   )
 }
