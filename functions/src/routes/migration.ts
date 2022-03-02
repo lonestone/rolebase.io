fd70788686832a0847d17de03616646c1d8dc5d8
Kérinec Fabien <fabien@lonestone.studio>
2022-03-02 16:17:09 +0100

    migration file for setting null on meetingId Log

--- functions/src/routes/migration.ts
+++ functions/src/routes/migration.ts
@@ -1,8 +1,22 @@
 import * as express from 'express'
-
+import * as admin from 'firebase-admin'
 // Migration script
 // http://localhost:5001/roles-app-37879/us-central1/api/migration
 
 export const migration: express.RequestHandler = async (req, res) => {
   res.send('ok')
+  await admin
+    .firestore()
+    .collection('logs')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.update({ meetingId: null }).catch((err) => {
+          console.warn(err)
+        })
+      })
+    })
+    .catch((err) => {
+      console.warn(err)
+    })
 }
