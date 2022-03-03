77c0a6c921ddcf6845c84eac9a68667fad3d3f65
Kérinec Fabien <fabien@lonestone.studio>
2022-03-03 10:44:24 +0100

    migration fix

--- functions/src/routes/migration.ts
+++ functions/src/routes/migration.ts
@@ -4,19 +4,12 @@ import * as admin from 'firebase-admin'
 // http://localhost:5001/roles-app-37879/us-central1/api/migration
 
 export const migration: express.RequestHandler = async (req, res) => {
-  res.send('ok')
-  await admin
-    .firestore()
-    .collection('logs')
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        doc.ref.update({ meetingId: null }).catch((err) => {
-          console.warn(err)
-        })
-      })
-    })
-    .catch((err) => {
+  const querySnapshot = await admin.firestore().collection('members').get()
+
+  querySnapshot.forEach((doc) => {
+    doc.ref.update({ meetingId: null }).catch((err) => {
       console.warn(err)
     })
+  })
+  res.send('ok')
 }
