e6df3c13fc3f4a457d532936a1229351001e02af
Godefroy <git@skreo.net>
2022-06-01 01:28:27 +0200

    Fix auth loading

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,9 +1,23 @@
+import Loading from '@components/atoms/Loading'
 import { useStoreState } from '@store/hooks'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import PrivateRoutes from './PrivateRoutes'
 import PublicRoutes from './PublicRoutes'
 
+// Show auth loading only if not in some routes
+const noLoadingRoutes = ['/login', '/signup']
+
 export default function Routes() {
   const user = useStoreState((state) => state.auth.user)
-  return user ? <PrivateRoutes /> : <PublicRoutes />
+  const loading = useStoreState((state) => state.auth.loading)
+  const route = useHistory()
+
+  return loading && !noLoadingRoutes.includes(route.location.pathname) ? (
+    <Loading center active />
+  ) : user ? (
+    <PrivateRoutes />
+  ) : (
+    <PublicRoutes />
+  )
 }
