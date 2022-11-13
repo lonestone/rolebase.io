b728e2dfc884b316feaab2456571c4a6710d2844
Godefroy <git@skreo.net>
2022-11-13 23:36:45 +0100

    Remove Firebase completely (and migration script)

--- functions/routes/replaceOldIds.ts
+++ functions/routes/replaceOldIds.ts
@@ -1,7 +1,8 @@
+import { gql } from '@gql'
+import { adminRequest } from '@utils/adminRequest'
 import { guardBodyParams } from '@utils/guardBodyParams'
 import { route } from '@utils/route'
 import * as yup from 'yup'
-import { oldIds, retrieveOldIds } from '_migration/oldIds'
 
 const yupSchema = yup.object().shape({
   text: yup.string().required(),
@@ -11,13 +12,25 @@ const yupSchema = yup.object().shape({
 // It's useful to convert urls from Firebase to Nhost
 export default route(async (context): Promise<string> => {
   const { text } = guardBodyParams(context, yupSchema)
-  if (oldIds.size === 0) {
-    await retrieveOldIds()
+
+  // Get old ids
+  const result = await adminRequest(GET_OLD_IDS)
+
+  // Replace old ids with new ids in provided text
+  let newText = text
+  for (const { id, oldId } of result.old_id) {
+    newText = newText.replaceAll(oldId, id)
   }
 
-  let newText = text
-  for (const [oldId, { newId }] of oldIds) {
-    newText = newText.replaceAll(oldId, newId)
-  }
   return newText
 })
+
+const GET_OLD_IDS = gql(`
+  query GetOldIds {
+    old_id {
+      id
+      oldId
+      type
+    }
+  }
+`)
