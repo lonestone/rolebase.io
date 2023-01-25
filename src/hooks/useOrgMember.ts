b3458fee0391f3ca2974c52c72958bd17625ef84
Noé Grangier <noe@lonestone.io>
2023-01-25 17:22:22 +0100

    Updated permissions + fixes

--- src/hooks/useOrgMember.ts
+++ src/hooks/useOrgMember.ts
@@ -5,7 +5,9 @@ import useSuperAdmin from './useSuperAdmin'
 export default function useOrgMember(): boolean {
   const userRole = useOrgRole()
   const isMember =
-    userRole === Member_Role_Enum.Member || userRole === Member_Role_Enum.Admin
+    userRole === Member_Role_Enum.Member ||
+    userRole === Member_Role_Enum.Admin ||
+    userRole === Member_Role_Enum.Owner
   const isSuperAdmin = useSuperAdmin()
   return isMember || isSuperAdmin
 }
