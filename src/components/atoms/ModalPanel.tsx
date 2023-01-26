c3e542a59ee1a257c6bfc33d946380fa1d18eff9
Godefroy <git@skreo.net>
2023-01-26 23:36:05 +0100

    Fix ModalPanel

--- src/components/atoms/ModalPanel.tsx
+++ src/components/atoms/ModalPanel.tsx
@@ -1,47 +1,34 @@
-import {
-  ModalContextProvider,
-  ModalProps,
-  StylesProvider,
-  useModal,
-  useMultiStyleConfig,
-} from '@chakra-ui/react'
+import { Modal, ModalProps } from '@chakra-ui/react'
 import { SidebarContext } from '@contexts/SidebarContext'
 import React, { useContext } from 'react'
 import GlassBox from './GlassBox'
 
-// Inspired by https://github.com/chakra-ui/chakra-ui/blob/main/packages/modal/src/modal.tsx
-
 export const modalPanelWidth = 450
 
 export default function ModalPanel({
+  isOpen = true,
   children,
   ...props
-}: Omit<ModalProps, 'isOpen'>) {
-  const styles = useMultiStyleConfig('Modal', props)
-  const modal = useModal({ isOpen: true, ...props })
+}: ModalProps) {
   const sidebarContext = useContext(SidebarContext)
 
-  const context = {
-    ...modal,
-  }
-
   return (
-    <GlassBox
-      position="absolute"
-      bottom={0}
-      right={0}
-      w={`${modalPanelWidth}px`}
-      maxW="100vw"
-      h={sidebarContext?.height ? undefined : '100vh'}
-      maxH={`calc(100vh - ${sidebarContext?.height || 0}px)`}
-      overflow="auto"
-      zIndex={1}
-      borderLeftWidth={sidebarContext?.height ? 0 : '1px'}
-      borderTopWidth={sidebarContext?.height ? '1px' : 0}
-    >
-      <ModalContextProvider value={context}>
-        <StylesProvider value={styles}>{children}</StylesProvider>
-      </ModalContextProvider>
-    </GlassBox>
+    <Modal isOpen={isOpen} {...props}>
+      <GlassBox
+        position="absolute"
+        bottom={0}
+        right={0}
+        w={`${modalPanelWidth}px`}
+        maxW="100vw"
+        h={sidebarContext?.height ? undefined : '100vh'}
+        maxH={`calc(100vh - ${sidebarContext?.height || 0}px)`}
+        overflow="auto"
+        zIndex={1}
+        borderLeftWidth={sidebarContext?.height ? 0 : '1px'}
+        borderTopWidth={sidebarContext?.height ? '1px' : 0}
+      >
+        {children}
+      </GlassBox>
+    </Modal>
   )
 }
