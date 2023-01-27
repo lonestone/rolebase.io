7d60de1dd37dae236f3626104e8ea95b339fc01a
Noé Grangier <noe.grangier@gmail.com>
2023-01-27 17:16:52 +0100

    Fix useCopyUrl

--- src/hooks/useCopyUrl.ts
+++ src/hooks/useCopyUrl.ts
@@ -5,17 +5,16 @@ import { useTranslation } from 'react-i18next'
 export default function useCopyUrl(url: string | undefined) {
   const { t } = useTranslation()
 
-  // URL copy
-  const { hasCopied, onCopy } = useClipboard(url || '')
+  const { hasCopied, onCopy, setValue } = useClipboard(url || '')
   const toast = useToast()
 
   useEffect(() => {
+    setValue(url || '')
+  }, [url])
+
+  useEffect(() => {
     if (!hasCopied) return
-    toast({
-      title: t('common.toastCopied'),
-      status: 'info',
-      duration: 1500,
-    })
+    toast({ title: t('common.toastCopied'), status: 'info', duration: 1500 })
   }, [hasCopied])
 
   return onCopy
