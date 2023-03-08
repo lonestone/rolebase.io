b553636d38b9aa8960a43d5fe4c0b62c0624ff78
Godefroy <git@skreo.net>
2023-03-08 21:45:58 +0100

    Add colors palette to ColorController

--- src/components/atoms/ColorController.tsx
+++ src/components/atoms/ColorController.tsx
@@ -1,11 +1,19 @@
+import { TriangleDownIcon } from '@chakra-ui/icons'
 import {
   Checkbox,
+  Flex,
+  Grid,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
   Slider,
   SliderFilledTrack,
   SliderThumb,
   SliderTrack,
 } from '@chakra-ui/react'
-import React from 'react'
+import { useStoreState } from '@store/hooks'
+import React, { useMemo } from 'react'
 import { Control, Controller, FieldValues, Path } from 'react-hook-form'
 import { defaultCircleColorHue } from 'src/theme'
 
@@ -15,11 +23,29 @@ interface Props<Values extends FieldValues> {
   children: string
 }
 
+const hueToColor = (hue: number) => `hsl(${hue} 97% 75%)`
+
 export default function ColorController<Values extends FieldValues>({
   name,
   control,
   children,
 }: Props<Values>) {
+  const circles = useStoreState((state) => state.org.circles)
+
+  // Determines colors palette from circles colors
+  const palette = useMemo(
+    () =>
+      circles &&
+      [
+        ...new Set(
+          circles
+            .map((circle) => circle.role.colorHue)
+            .filter(Boolean) as number[]
+        ),
+      ].sort((a, b) => a - b),
+    [circles]
+  )
+
   return (
     <Controller
       name={name}
@@ -35,24 +61,57 @@ export default function ColorController<Values extends FieldValues>({
             {children}
           </Checkbox>
           {typeof field.value === 'number' && (
-            <Slider
-              aria-label=""
-              min={0}
-              max={360}
-              step={1}
-              defaultValue={defaultCircleColorHue}
-              value={field.value as number}
-              mt={3}
-              onChange={(value) => field.onChange(value)}
-            >
-              <SliderTrack bgGradient="linear(to-r, #ff0000, #ffff00, #00ff00, #00ffff, #0000ff, #ff00ff, #ff0000)">
-                <SliderFilledTrack bg="transparent" />
-              </SliderTrack>
-              <SliderThumb
-                boxSize="1.8rem"
-                bg={`hsl(${field.value} 97% 75%)`}
-              />
-            </Slider>
+            <Flex alignItems="center">
+              <Slider
+                aria-label=""
+                min={0}
+                max={360}
+                step={1}
+                defaultValue={defaultCircleColorHue}
+                value={field.value as number}
+                my={3}
+                onChange={(value) => field.onChange(value)}
+              >
+                <SliderTrack bgGradient="linear(to-r, #ff0000, #ffff00, #00ff00, #00ffff, #0000ff, #ff00ff, #ff0000)">
+                  <SliderFilledTrack bg="transparent" />
+                </SliderTrack>
+                <SliderThumb boxSize="1.8rem" bg={hueToColor(field.value)} />
+              </Slider>
+
+              {palette && palette.length !== 0 && (
+                <Menu placement="bottom-end">
+                  <MenuButton
+                    as={IconButton}
+                    aria-label="Palette"
+                    variant="ghost"
+                    size="sm"
+                    color={hueToColor(field.value)}
+                    icon={<TriangleDownIcon />}
+                    ml={2}
+                  />
+                  <MenuList px={2}>
+                    <Grid
+                      gap={2}
+                      templateColumns={`repeat(${Math.min(
+                        6,
+                        palette.length
+                      )}, 1fr)`}
+                    >
+                      {palette?.map((hue) => (
+                        <IconButton
+                          key={hue}
+                          aria-label=""
+                          icon={<></>}
+                          bg={hueToColor(hue)}
+                          _hover={{ bg: hueToColor(hue), borderWidth: 2 }}
+                          onClick={() => field.onChange(hue)}
+                        />
+                      ))}
+                    </Grid>
+                  </MenuList>
+                </Menu>
+              )}
+            </Flex>
           )}
         </>
       )}
