45b215cc3b1d9ebe087e3271382d365dca5a70f0
Noé Grangier <noe.grangier@gmail.com>
2023-03-13 17:47:46 +0100

    Fixed display for org with free subscription

--- src/components/molecules/subscription/SubscriptionUpcomingInvoiceCard.tsx
+++ src/components/molecules/subscription/SubscriptionUpcomingInvoiceCard.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardProps,
@@ -26,40 +27,54 @@ export default function SubscriptionUpcomingInvoiceCard({
 
   return (
     <>
-      <Card p="4" variant="outline" {...cardProps}>
-        <Text fontSize={16} fontWeight={700}>
-          {t('SubscriptionPlans.upcomingInvoiceAmount')}
-        </Text>
-        <Flex
-          p="4"
-          w="100%"
-          gap="5"
-          justifyContent="space-between"
-          flexDir="row"
-          flexWrap={['wrap', 'nowrap']}
-        >
-          <Flex flexDir="column" alignItems="start" justifyContent="flex-start">
-            <Text color="gray.500" as="i" fontSize={14} fontWeight={500}>
-              {t('SubscriptionTabs.invoiceTab.amount')}
-            </Text>
-            <Text fontSize={26} fontWeight={700} mt="-2">
-              {(upcomingInvoice.totalInCents / 100).toFixed(2)}€
-            </Text>
-          </Flex>
-
+      <Card
+        as={Flex}
+        flexDir="column"
+        justifyContent="space-between"
+        p="4"
+        variant="outline"
+        {...cardProps}
+      >
+        <Box>
+          <Text fontSize={16} fontWeight={700}>
+            {t('SubscriptionPlans.upcomingInvoiceAmount')}
+          </Text>
           <Flex
-            flexDir="column"
-            alignItems={['start', 'end']}
-            justifyContent={['flex-start', 'flex-end']}
+            p="4"
+            w="100%"
+            gap="5"
+            justifyContent="space-between"
+            flexDir="row"
+            flexWrap={['wrap', 'nowrap']}
           >
-            <Text color="gray.500" as="i" fontSize={14} fontWeight={500}>
-              {t('SubscriptionPlans.drawdownOn')}
-            </Text>
-            <Text fontSize={26} fontWeight={700} mt="-2">
-              {format(new Date(upcomingInvoice.nextPayment), 'dd/MM/uuuu')}
-            </Text>
+            <Flex
+              flexDir="column"
+              alignItems="start"
+              justifyContent="flex-start"
+            >
+              <Text color="gray.500" as="i" fontSize={14} fontWeight={500}>
+                {t('SubscriptionTabs.invoiceTab.amount')}
+              </Text>
+              <Text fontSize={26} fontWeight={700} mt="-2">
+                {(upcomingInvoice.totalInCents / 100).toFixed(2)}€
+              </Text>
+            </Flex>
+
+            <Flex
+              flexDir="column"
+              alignItems={['start', 'end']}
+              justifyContent={['flex-start', 'flex-end']}
+            >
+              <Text color="gray.500" as="i" fontSize={14} fontWeight={500}>
+                {t('SubscriptionPlans.drawdownOn')}
+              </Text>
+              <Text fontSize={26} fontWeight={700} mt="-2">
+                {format(new Date(upcomingInvoice.nextPayment), 'dd/MM/uuuu')}
+              </Text>
+            </Flex>
           </Flex>
-        </Flex>
+        </Box>
+
         <Button
           variant="ghost"
           justifyContent="center"
