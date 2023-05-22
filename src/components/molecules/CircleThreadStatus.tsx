3f86db6bfec69400a5559e09c96c143b380adc04
Chloengr <chloe.nger@gmail.com>
2023-05-22 11:57:15 +0200

    fix from review

--- src/components/molecules/CircleThreadStatus.tsx
+++ src/components/molecules/CircleThreadStatus.tsx
@@ -1,4 +1,4 @@
-import { Circle, Tooltip } from '@chakra-ui/react'
+import { BoxProps, Circle, Tooltip } from '@chakra-ui/react'
 import { Thread_Status_Enum } from '@gql'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -8,7 +8,10 @@ export type CircleThreadStatusProps = {
   status: Thread_Status_Enum
 }
 
-export const CircleThreadStatus = ({ status }: CircleThreadStatusProps) => {
+export const CircleThreadStatus = ({
+  status,
+  ...boxProps
+}: CircleThreadStatusProps) => {
   const { t } = useTranslation()
 
   return (
