4a6087c84c15aece333810ef88845a177b644773
Chloengr <chloe.nger@gmail.com>
2023-05-22 11:57:15 +0200

    fix migrations

--- nhost/migrations/default/1684140261254_squashed/down.sql
+++ nhost/migrations/default/1684140261254_squashed/down.sql
@@ -1,21 +1,17 @@
 
 alter table "public"."thread" drop constraint "thread_status_fkey";
 
-alter table "public"."thread" alter column "status" drop not null;
-
 -- Could not auto-generate a down migration.
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."thread" add column "status" text
---  null default 'Preparation';
-
-DELETE FROM "public"."thread_status" WHERE "value" = 'Done';
+--  not null default 'Preparation';
 
 DELETE FROM "public"."thread_status" WHERE "value" = 'Closed';
 
-DELETE FROM "public"."thread_status" WHERE "value" = 'Blocked';
-
 DELETE FROM "public"."thread_status" WHERE "value" = 'Preparation';
 
-alter table "public"."thread_status" alter column "comment" set not null;
+DELETE FROM "public"."thread_status" WHERE "value" = 'Blocked';
+
+DELETE FROM "public"."thread_status" WHERE "value" = 'Active';
 
 DROP TABLE "public"."thread_status";
