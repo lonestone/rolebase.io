41e3aca05b3c1963e6c5f1e91dbbc4fc0ea6625b
Godefroy <git@skreo.net>
2023-06-25 01:08:58 +0200

    Fix nhost client in functions

--- functions/_utils/nhost.ts
+++ functions/_utils/nhost.ts
@@ -2,6 +2,7 @@ import { NhostClient } from '@nhost/nhost-js'
 
 const nhost = new NhostClient({
   subdomain: process.env.NHOST_SUBDOMAIN,
+  region: process.env.NHOST_REGION,
   adminSecret: process.env.NHOST_ADMIN_SECRET,
 })
 
