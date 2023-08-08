7095b2d33f675a9663f5ae59585356fa0ace1077
Godefroy <git@skreo.net>
2023-08-08 23:39:21 +0200

    Reorganize some menus

--- src/components/molecules/circle/CirclesSettings.tsx
+++ src/components/molecules/circle/CirclesSettings.tsx
@@ -1,30 +1,52 @@
-import IconTextButton from '@atoms/IconTextButton'
+import IconTextButton, { IconTextButtonProps } from '@atoms/IconTextButton'
 import {
-  ButtonProps,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   useDisclosure,
+  useMediaQuery,
 } from '@chakra-ui/react'
+import { useNavigateOrg } from '@hooks/useNavigateOrg'
 import useOrgAdmin from '@hooks/useOrgAdmin'
 import useOrgMember from '@hooks/useOrgMember'
+import CirclesKeyboardShortcutsModal from '@organisms/circle/CirclesKeyboardShortcutsModal'
 import OrgEditModal from '@organisms/org/OrgEditModal'
 import BaseRolesModal from '@organisms/role/BaseRolesModal'
 import VacantRolesModal from '@organisms/role/VacantRolesModal'
-import React from 'react'
+import { useStoreState } from '@store/hooks'
+import React, { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { FiSettings } from 'react-icons/fi'
+import {
+  FiCircle,
+  FiCommand,
+  FiDownload,
+  FiRotateCcw,
+  FiSettings,
+  FiShare2,
+} from 'react-icons/fi'
 
-export default function CirclesSettings(buttonProps: ButtonProps) {
+export default function CirclesSettings(
+  buttonProps: Omit<IconTextButtonProps, 'aria-label'>
+) {
   const { t } = useTranslation()
+  const navigateOrg = useNavigateOrg()
   const isMember = useOrgMember()
   const isAdmin = useOrgAdmin()
+  const [isMobile] = useMediaQuery('(max-width: 600px)')
+
+  // Get org circle id
+  const circles = useStoreState((state) => state.org.circles)
+  const orgCircleId = useMemo(() => {
+    if (!circles) return undefined
+    return circles.find((c) => c.parentId === null)?.id
+  }, [circles])
 
   // Modals
   const orgEditModal = useDisclosure()
   const baseRolesModal = useDisclosure()
   const vacantRolesModal = useDisclosure()
+  const shortcutsModal = useDisclosure()
 
   return (
     <Menu>
@@ -32,7 +54,7 @@ export default function CirclesSettings(buttonProps: ButtonProps) {
         as={IconTextButton}
         icon={<FiSettings />}
         aria-label={t('CirclesSettings.label')}
-        className="userflow-graph-views"
+        className="userflow-settings"
         {...buttonProps}
       />
 
@@ -40,15 +62,35 @@ export default function CirclesSettings(buttonProps: ButtonProps) {
         {isMember && (
           <>
             {isAdmin && (
-              <MenuItem onClick={orgEditModal.onOpen}>
+              <MenuItem icon={<FiSettings />} onClick={orgEditModal.onOpen}>
                 {t('CirclesSettings.orgEdit')}
               </MenuItem>
             )}
-            <MenuItem onClick={baseRolesModal.onOpen}>
+            <MenuItem icon={<FiCircle />} onClick={baseRolesModal.onOpen}>
               {t('CirclesSettings.baseRoles')}
             </MenuItem>
-            <MenuItem onClick={vacantRolesModal.onOpen}>
+            <MenuItem icon={<FiCircle />} onClick={vacantRolesModal.onOpen}>
               {t('CirclesSettings.vacantRoles')}
+            </MenuItem>
+            <MenuItem
+              icon={<FiRotateCcw />}
+              onClick={() => navigateOrg('logs')}
+            >
+              {t('CirclesSettings.logs')}
+            </MenuItem>
+            {isMember && !isMobile && (
+              <MenuItem icon={<FiCommand />} onClick={shortcutsModal.onOpen}>
+                {t('CirclesSettings.shortcuts')}
+              </MenuItem>
+            )}
+            <MenuItem
+              icon={<FiDownload />}
+              onClick={() => navigateOrg(`export-circle/${orgCircleId}`)}
+            >
+              {t('CirclesSettings.export')}
+            </MenuItem>
+            <MenuItem icon={<FiShare2 />} onClick={() => navigateOrg('share')}>
+              {t('CirclesSettings.embed')}
             </MenuItem>
           </>
         )}
@@ -64,6 +106,13 @@ export default function CirclesSettings(buttonProps: ButtonProps) {
 
       {vacantRolesModal.isOpen && (
         <VacantRolesModal isOpen onClose={vacantRolesModal.onClose} />
+      )}
+
+      {shortcutsModal.isOpen && (
+        <CirclesKeyboardShortcutsModal
+          isOpen
+          onClose={shortcutsModal.onClose}
+        />
       )}
     </Menu>
   )
