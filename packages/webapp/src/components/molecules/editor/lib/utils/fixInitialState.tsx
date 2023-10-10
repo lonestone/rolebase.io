5cb98759157e42ba9b2bb1ebbdc84560de6b3d57
Godefroy <git@skreo.net>
2023-10-10 23:21:23 +0200

    Fix error when editor state.root.children is empty

--- packages/webapp/src/components/molecules/editor/lib/utils/fixInitialState.tsx
+++ packages/webapp/src/components/molecules/editor/lib/utils/fixInitialState.tsx
@@ -6,10 +6,26 @@ export function fixInitialState(
   value?: InitialEditorStateType
 ): InitialEditorStateType | undefined {
   if (!value) return undefined
+
   // Markdown
   if (typeof value === 'string' && value[0] !== '{') {
     return () => $convertFromMarkdownString(value, markdownTransformers)
   }
+
   // Other types: EditorState, string, function
+
+  // Handle possible errors in JSON
+  try {
+    if (typeof value === 'string') {
+      const state = JSON.parse(value)
+      if (!state?.root?.children?.length) {
+        throw new Error('Empty children in state')
+      }
+    }
+  } catch (error) {
+    console.warn(error)
+    return undefined
+  }
+
   return value
 }
