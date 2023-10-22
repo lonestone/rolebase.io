57f2efdde609a88895957a1b9100968418263cd6
Godefroy <git@skreo.net>
2023-10-22 21:30:57 +0200

    Fix migration script for office365

--- functions/routes/apps/office365/migrate.ts
+++ functions/routes/apps/office365/migrate.ts
@@ -1,29 +1,39 @@
 import { gql } from '@gql'
 import { adminRequest } from '@utils/adminRequest'
+import { guardQueryParams } from '@utils/guardQueryParams'
 import { route } from '@utils/route'
+import * as yup from 'yup'
 import Office365App from './_Office365App'
+
+const yupSchema = yup.object().shape({
+  appId: yup.string().required(),
+})
 
 // Migrate legacy calendars
 // (remove this route after execution)
 export default route(async (context) => {
-  const { user_app } = await adminRequest(
+  const { appId } = guardQueryParams(context, yupSchema)
+
+  const { user_app_by_pk: userApp } = await adminRequest(
     gql(`
-      query getUserApps {
-        user_app {
+      query getUserApps($id: uuid!) {
+        user_app_by_pk(id: $id) {
           ...UserAppFull
         }
       }
-    `)
+    `),
+    { id: appId }
   )
-
-  for (const userApp of user_app) {
-    try {
-      await new Office365App(userApp).migrateLegacyCalendars()
-      console.log(`Migrated user app ${userApp.id}`)
-    } catch (e) {
-      console.log(`Error migrating user app ${userApp.id}`, e)
-    }
+  if (!userApp) {
+    return 'App not found'
   }
 
-  return 'Migration OK'
+  try {
+    await new Office365App(userApp).migrateLegacyCalendars()
+    console.log(`Migrated user app ${userApp.id}`)
+    return `Migrated user app ${userApp.id}`
+  } catch (e) {
+    console.log(`Error migrating user app ${userApp.id}`, e)
+    return `Error migrating user app ${userApp.id}`
+  }
 })
