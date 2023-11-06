488bb1d53e3f31fc40b3807e09c3cb7ec2813a1d
Godefroy <git@skreo.net>
2023-11-07 00:00:33 +0100

    Add migration for GoogleCalendar app type

---
+++ nhost/migrations/default/1699311330141_run_sql_migration/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT INTO "public"."app_type"("value", "comment") VALUES (E'GoogleCalendar', null);
---
+++ nhost/migrations/default/1699311330141_run_sql_migration/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."app_type"("value", "comment") VALUES (E'GoogleCalendar', null);
